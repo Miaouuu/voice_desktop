3cfee4e579021fe6a69f2ec841213e198d339be9
Ruben Barros <atosc08@gmail.com>
2020-06-16 21:29:08 +0200

    Channel

--- src/views/Server.vue
+++ src/views/Server.vue
@@ -2,7 +2,26 @@
   <div>
     <p>{{server.name}}</p>
     <div v-if="userId === server.creator">
-      <p>Create</p>
+      <p @click="isVisible = !isVisible">{{isVisible ? "-" : "+"}}</p>
+      <form v-if="isVisible" @submit.prevent="createChannel(name, selected, server._id)">
+        <input type="text" v-model="name" />
+        <select v-model="selected">
+          <option v-for="selection in selections" :key="selection">{{selection}}</option>
+        </select>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+    <div>
+      <div v-for="channel in server.channels" :key="channel._id">
+        <p @click="showChannel(channel._id)">{{channel.name}}</p>
+      </div>
+    </div>
+    <div v-if="channelSelected.selected">
+      <p>{{channelSelected.name}}</p>
+      <form @submit.prevent="sendMessage(message)">
+        <input type="text" v-model="message" />
+        <button type="submit">Send</button>
+      </form>
     </div>
   </div>
 </template>
@@ -11,7 +30,18 @@ export default {
   name: "Server",
   data() {
     return {
-      server: {}
+      server: {},
+      isVisible: false,
+      name: "",
+      selected: "TEXT",
+      selections: ["TEXT", "VOICE", "BOTH"],
+      message: "",
+      channelSelected: {
+        selected: false,
+        id: "",
+        name: "",
+        messages: []
+      }
     };
   },
   mounted() {
@@ -19,6 +49,30 @@ export default {
       this.$http
         .get("http://localhost:3000/api/server/" + this.$route.params.id)
         .then(data => (this.server = data.data));
+    }
+  },
+  methods: {
+    createChannel(name, selected, serverId) {
+      this.$http
+        .post("http://localhost:3000/api/channel/", {
+          name: name,
+          server: serverId,
+          type: selected
+        })
+        .then(data => (this.server = data.data));
+      this.name = "";
+    },
+    showChannel(id) {
+      this.channelSelected.selected = true;
+      this.$http.get("http://localhost:3000/api/channel/" + id).then(data => {
+        this.channelSelected.name = data.data.name;
+        this.channelSelected.id = data.data._id;
+        this.channelSelected.messages = data.data.messages;
+      });
+    },
+    sendMessage(message) {
+      this.message = "";
+      console.log(message);
     }
   },
   computed: {
